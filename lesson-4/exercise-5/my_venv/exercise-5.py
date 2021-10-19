ed11aea8d51d962902e08e3071a01f9fba01fca0
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-19 13:30:20 +0300

    Modify exercise-5.py

--- lesson-4/exercise-5/my_venv/exercise-5.py
+++ lesson-4/exercise-5/my_venv/exercise-5.py
@@ -5,5 +5,9 @@
 5. * (вместо 4) Доработать скрипт из предыдущего задания: теперь он должен работать и из консоли. Например:
 > python task_4_5.py USD
 75.18, 2020-09-05
-
 """
+import sys
+from utils import currency_rates
+
+my_dict = currency_rates(sys.argv[1:][0])
+print(my_dict)
