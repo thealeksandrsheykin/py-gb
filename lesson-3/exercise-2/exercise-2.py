51bb8e2c5766d83a101e27656460603cc94a60db
Aleksandr <alexander_sheykin@mail.ru>
2021-10-23 22:58:42 +0300

    Update exercise-2.py

--- lesson-3/exercise-2/exercise-2.py
+++ lesson-3/exercise-2/exercise-2.py
@@ -5,25 +5,28 @@
 2. * (вместо задачи 1) Доработать предыдущую функцию в num_translate_adv(): реализовать корректную работу с
 числительными, начинающимися с заглавной буквы — результат тоже должен быть с заглавной. Например:
 num_translate_adv("One") "Один"
-num_translate_adv("Two") "Два"
+num_translate_adv("two") "два"
 
 """
 
+numbers = {'one': 'один',
+           'two': 'два',
+           'three': 'три',
+           'four': 'четыре',
+           'five': 'пять',
+           'six': 'шесть',
+           'seven': 'семь',
+           'eight': 'восемь',
+           'nine': 'девять',
+           'ten': 'десять'}
+
 
 def num_translate_adv(data):
-    numbers = {'one': 'один',
-               'two': 'два',
-               'three': 'три',
-               'four': 'четыре',
-               'five': 'пять',
-               'six': 'шесть',
-               'seven': 'семь',
-               'eight': 'восемь',
-               'nine': 'девять',
-               'ten': 'десять'}
-
-    return numbers[data]
+    if data[0] == data[0].upper():
+        return (numbers.get(data.lower())).capitalize()
+    else:
+        return numbers.get(data)
 
 
 number = input('Введите число на английском от 1-10: ')
-print(f'"{(num_translate_adv(number.lower())).capitalize()}"')
+print(f'"{num_translate_adv(number)}"')
