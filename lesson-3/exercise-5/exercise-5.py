e22bd81910f390c3b12d36c50eab9c0278db675f
Aleksandr <alexander_sheykin@mail.ru>
2021-10-24 00:03:47 +0300

    Modify exercise-5.py

--- lesson-3/exercise-5/exercise-5.py
+++ lesson-3/exercise-5/exercise-5.py
@@ -25,23 +25,27 @@ adverbs = ["сегодня", "вчера", "завтра", "позавчера",
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(**kwargs):
+def get_jokes_1(number, repeats=True):
     my_list1 = list()
     my_list2 = list()
     # Делаем цикл на количество необходимы шуток
-    for i in range(0, kwargs['number']):
+    for i in range(0, number):
         joke = ''
         for j in [nouns, adverbs, adjectives]:
             # Рандомно забираем одно слово из каждого списка и присваиваем к переменной тем самым составляя предложение
-            if kwargs['flag']:
-                # Если флаг = True, то каждое слово выбранное рандомно добавляем в список чтобы далее сравнивать с ним
-                while True:
-                    word = random.choice(j)
-                    if not (word in my_list2):
-                        my_list2.append(word)
-                        break
-                    else:
-                        pass
+            if not repeats:
+                if number > min(len(nouns), len(adverbs), len(adjectives)):
+                    return 'No way'
+                else:
+                    # Если флаг = True, то каждое слово выбранное рандомно добавляем в список чтобы далее
+                    # сравнивать с ним
+                    while True:
+                        word = random.choice(j)
+                        if not (word in my_list2):
+                            my_list2.append(word)
+                            break
+                        else:
+                            pass
             else:
                 word = random.choice(j)
             joke += f'{word} '
@@ -49,4 +53,25 @@ def get_jokes(**kwargs):
     return my_list1
 
 
-print(f'{get_jokes(number=4, flag=True)}')
+def get_jokes_2(number, repeats=True):
+    my_list = []
+    if not repeats:
+        if number > min(len(nouns), len(adverbs), len(adjectives)):
+            return 'No way'
+        else:
+            random.shuffle(nouns)
+            random.shuffle(adverbs)
+            random.shuffle(adjectives)
+            for i in range(number):
+                my_list.append(f'{nouns[i]} {adverbs[i]} {adjectives[i]}')
+    else:
+        for i in range(number):
+            cur_nouns = random.choice(nouns)
+            cur_adverbs = random.choice(adverbs)
+            cur_adjectives = random.choice(adjectives)
+            my_list.append(f'{cur_nouns} {cur_adverbs} {cur_adjectives}')
+    return my_list
+
+
+print(f'{get_jokes_1(4, repeats=False)}')
+print(f'{get_jokes_2(5, repeats=True)}')
