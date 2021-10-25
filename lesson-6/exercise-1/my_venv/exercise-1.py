83425655fddfc6d3819a0280316a1b992aa3507f
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-25 14:21:43 +0300

    Create exercise-1.py

---
+++ lesson-6/exercise-1/my_venv/exercise-1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+
+"""
+1.Не используя библиотеки для парсинга, распарсить (получить определённые данные) файл логов web-сервера nginx_logs.txt
+(https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs)—получить список кортежей
+ вида:
+ (<remote_addr>, <request_type>, <requested_resource>). Например:
+
+[
+...
+('141.138.90.60', 'GET', '/downloads/product_2'),
+('141.138.90.60', 'GET', '/downloads/product_2'),
+('173.255.199.22', 'GET', '/downloads/product_2'),
+...
+]
+
+Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
+
+"""
+import requests
+
+URL = 'https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs'
+
+responce = requests.get(URL).text
