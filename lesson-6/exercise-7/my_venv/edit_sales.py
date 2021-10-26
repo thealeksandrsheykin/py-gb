1970f37d40956514734ca1f240a7dd7027c006c1
Aleksandr <alexander_sheykin@mail.ru>
2021-10-27 00:17:54 +0300

    Modify edit_sales.py

--- lesson-6/exercise-7/my_venv/edit_sales.py
+++ lesson-6/exercise-7/my_venv/edit_sales.py
@@ -5,22 +5,40 @@ import sys
 from fileinput import FileInput
 
 
-def edit_sales(row,value):
-    with open('bakery.csv', mode='rt+') as file:
-        file_position = 0
-        for i,j in enumerate(file,start=1):
+def edit_sales(row, value):
+    flag_change = False
+    with open('bakery.csv', mode='r+') as file:
+        with open('bakery.tmp', mode='w+') as file_temp:
+            for i, j in enumerate(file, 1):
+                if i == int(row):
+                    flag_change = True
+                    file_temp.write(f'{value}\n')
+                else:
+                    file_temp.write(j)
+            if not flag_change:
+                sys.exit(1)
+            file_temp.seek(0)
+            file.seek(0)
+            for line in file_temp:
+                file.write(line)
+
+
+def edit_sales_adv(row, value):
+    flag_change = False
+    with FileInput(files='bakery.csv', inplace=True, ) as file:
+        for i, j in enumerate(file, 1):
             if i == int(row):
-                break
+                flag_change = True
+                print(f'{value}')
             else:
-                file_position += len(j)
-        print(file_position)
-        file.seek(file_position)
-        file.write(value)
-        file.write('\n')
+                print(f'{j.strip()}')
+        if not flag_change:
+            sys.exit(1)
 
 
 if len(sys.argv) > 3:
     sys.exit(1)
 else:
-    row,value = sys.argv[1:]
-    edit_sales(row,value)
+    row, value = sys.argv[1:]
+    # edit_sales(row, value)
+    edit_sales_adv(row, value)
