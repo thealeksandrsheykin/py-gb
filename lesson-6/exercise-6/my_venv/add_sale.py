3cd62c99196ac9e3176188e05e7b49b9ae6b361b
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-26 15:39:43 +0300

    Modify add_sale.py show_sales.py exercise-6.py

--- lesson-6/exercise-6/my_venv/add_sale.py
+++ lesson-6/exercise-6/my_venv/add_sale.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python3
 
+import sys
+import csv
+
+def add_sale(summa):
+    with open('bakery.csv', mode='a+', newline='') as file_write:
+        writer = csv.writer(file_write, delimiter=';', lineterminator='\n')
+        writer.writerow([summa])
+
+if len(sys.argv) > 2:
+    sys.exit(1)
+else:
+    add_sale(sys.argv[1])
--- lesson-6/exercise-6/my_venv/show_sales.py
+++ lesson-6/exercise-6/my_venv/show_sales.py
@@ -2,3 +2,24 @@
 # !/usr/bin/env python3
 
 
+import sys
+from itertools import islice
+
+
+def show_sales(start,stop):
+    with open('bakery.csv', mode='r') as file:
+        for row in islice(file, start, stop):
+            print(f'{row.strip()}')
+
+if len(sys.argv[1:]) == 0:
+    start,stop = None,None
+elif len(sys.argv[1:]) == 1:
+    start,stop = int(sys.argv[1:][0]) - 1,None
+elif len(sys.argv[1:]) == 2:
+    start,stop = [int(i) for i in sys.argv[1:]]
+    start -= 1
+else:
+    sys.exit(1)
+
+show_sales(start,stop)
+
