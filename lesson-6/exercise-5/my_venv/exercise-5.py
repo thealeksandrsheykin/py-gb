e2195fe50c20b908de4101c2b5d57890a07b8fe1
Aleksandr <alexander_sheykin@mail.ru>
2021-10-26 22:49:00 +0300

    Modify exercise-5.py

--- lesson-6/exercise-5/my_venv/exercise-5.py
+++ lesson-6/exercise-5/my_venv/exercise-5.py
@@ -2,6 +2,24 @@
 # !/usr/bin/env python3
 
 """
-**(вместо 4) Решить задачу 4 и реализовать интерфейс командной строки, чтобы можно было задать имя обоих исходных файлов и имя выходного файла. Проверить работу скрипта.
-
+**(вместо 4) Решить задачу 4 и реализовать интерфейс командной строки, чтобы можно было задать имя обоих исходных файлов
+и имя выходного файла. Проверить работу скрипта.
 """
+
+import sys
+from itertools import zip_longest
+
+users,hobby,result = sys.argv[1:]
+
+with open(result, mode= 'w+', encoding='utf-8') as file:
+    with open(users, mode='r', encoding='utf-8') as users:
+        users_num= sum(1 for _ in users)
+        with open(hobby, mode='r', encoding='utf-8') as hobby:
+            hobby_num = sum(1 for _ in hobby)
+            if users_num < hobby_num:
+                sys.exit(1)
+            else:
+                users.seek(0)
+                hobby.seek(0)
+                for i,j in zip_longest(users,hobby):
+                    file.write(f"{' '.join(i.strip().split(','))}: {j}")
