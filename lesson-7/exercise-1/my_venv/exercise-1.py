b75a6ca6c169e2c64e71bea0044c6b5c6d83f1d6
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-27 20:05:27 +0300

    Modify exercise-1.py

--- lesson-7/exercise-1/my_venv/exercise-1.py
+++ lesson-7/exercise-1/my_venv/exercise-1.py
@@ -14,3 +14,28 @@
 можно ли будет при этом расширять конфигурацию и хранить данные о вложенных папках и файлах (добавлять детали)?
 
 """
+
+import os
+import json
+
+path = os.getcwd()
+print(f'Рабочий каталог: {path}')
+
+# Прямой способ решения данной задачи
+"""
+folder = 'my_project'
+subfolders = ('settings', 'mainapp', 'adminapp', 'authapp')
+
+for i in subfolders:
+    dir_path = os.path.join(folder, i)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+"""
+
+# Забираем структуру данных из файла JSON
+with open('structura.json', 'r') as file:
+    my_dict = json.load(file)
+    for key,value in my_dict.items():
+        if not os.path.exists(value['path']):
+            os.makedirs(value['path'])
+
