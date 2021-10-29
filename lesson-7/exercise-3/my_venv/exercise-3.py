99434258afca2d08408cd0da8b469bfa34ce1a39
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-29 15:30:15 +0300

    Modify exercise-3.py

--- lesson-7/exercise-3/my_venv/exercise-3.py
+++ lesson-7/exercise-3/my_venv/exercise-3.py
@@ -16,16 +16,25 @@
    |      |--index.html
 
 
-Примечание: исходные файлы необходимо оставить; обратите внимание, что html-файлы расположены в родительских папках (они
-играют роль пространств имён);
+Примечание: исходные файлы необходимо оставить;
+обратите внимание, что html-файлы расположены в родительских папках (они играют роль пространств имён);
+(Пространство имён позволяет отличить один элемент от другого, когда они имеют одинаковое название, но принадлежат разным
+пространствам имён.)
+
 предусмотреть возможные исключительные ситуации; это реальная задача, которая решена, например, во фреймворке django.
 """
 
 import os
+import shutil
 
-#root = os.getcwd()
-path = 'my_project'
+path = r'.\my_project'
 
 for root, dirs, files in os.walk(path):
-    print(files)
+    for name in files:
+        if name.endswith(".html"):
+            src = '\\'.join(root.split('\\')[:-1])
+            dst = os.path.join(path,'templates')
+            shutil.copytree(src,dst,dirs_exist_ok=True,ignore=shutil.ignore_patterns('*.txt','dir_for_ignore'))
+
+
 
