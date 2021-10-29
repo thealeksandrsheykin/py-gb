838911cd4093e7bcabd180b1f483eb2982f8d46b
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-10-29 19:41:46 +0300

    Modify exercise-5.py

--- lesson-7/exercise-5/my_venv/exercise-5.py
+++ lesson-7/exercise-5/my_venv/exercise-5.py
@@ -19,22 +19,30 @@ import os
 
 
 path = r'..\\'
+
 my_list = [100,1000,10000,100000]
-my_dict = dict.fromkeys(my_list,None)
-tmp_dict = dict.fromkeys(my_list,0)
+cnt = [0] * 4
+ext = [[]] * 4
 
 for root, dirs,files in os.walk(path):
         for file in files:
             filepath = os.path.join(root, file)
-            ext = os.path.splitext(filepath)[-1].lstrip('.')
             size = os.stat(filepath).st_size
+            extension = os.path.splitext(filepath)[-1].lstrip('.')
             try:
-                key = min(filter(lambda i: size < i, my_list))
+                value = min(filter(lambda i: size < i, my_list))
             except ValueError:
                 pass
-            tmp_dict[key] += 1
+            cnt[my_list.index(value)] += 1
+            if not extension in ext[my_list.index(value)]:
+                ext[my_list.index(value)].append(extension)
 
-print(tmp_dict)
+my_dict = dict(zip(my_list,zip(cnt,ext)))
+
+for i,j in my_dict.items():
+    print(f'{i:<10} : {j}')
+
+
 
 
 
