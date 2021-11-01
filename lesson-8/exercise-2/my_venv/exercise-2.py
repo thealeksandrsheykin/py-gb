a93017b214768d5f17c61c40750fd138165aface
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-11-01 17:53:02 +0300

    Modify exercise-2.py

--- lesson-8/exercise-2/my_venv/exercise-2.py
+++ lesson-8/exercise-2/my_venv/exercise-2.py
@@ -16,4 +16,30 @@ parsed_raw = ('188.138.60.101', '17/May/2015:08:05:49 +0000', 'GET', '/downloads
 
 """
 
+import re
+import requests
+
+def ParseUrl(data):
+    my_list =list()
+    regex_ipv4 = r'((?:\d+[\.]){3}\d+)(?:\s+\-){2}\s+\[(.*)\]\s+\"(\w+)\s+(\S+)\s+\S+\"\s+(\w+)\s+(\w+)\s+.*'
+    regex_ipv6 = r'((?:\w+[\:]{1,2}){1,7}\w+)(?:\s+\-){2}\s+\[(.*)\]\s+\"(\w+)\s+(\S+)\s+\S+\"\s+(\w+)\s+(\w+)\s+.*'
+    for i in data.split('\n'):
+        match_ipv4 = re.match(regex_ipv4, i)
+        match_ipv6 = re.match(regex_ipv6, i)
+        if match_ipv4:
+           my_list.append(match_ipv4.groups())
+        elif match_ipv6:
+            my_list.append(match_ipv6.groups())
+        else:
+            pass
+    return my_list
+
+if __name__ == '__main__':
+    url = r'https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs'
+    response = requests.get(url)
+    for i in ParseUrl(response.text):
+        print(f'{i}')
+
+
+
 
