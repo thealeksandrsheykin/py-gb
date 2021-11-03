88c3ce6e872f1e24b33e528b5d297bda68f2e005
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-11-03 19:05:55 +0300

    Modify exercise-3.py

--- lesson-9/exercise-3/my_venv/exercise-3.py
+++ lesson-9/exercise-3/my_venv/exercise-3.py
@@ -16,3 +16,36 @@
             проверить значения атрибутов,
             вызвать методы экземпляров.
 """
+
+class Worker:
+    def __init__(self,name,surname,position,wage,bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position # должность
+        self._income = {"wage": wage, "bonus": bonus}    # доход (оклад, премия)
+
+class Position(Worker):
+    def __init__(self,name,surname,position,wage,bonus):
+        super().__init__(name,surname,position,wage,bonus)
+
+    def get_full_name(self):
+        return f'{(self.name).capitalize()} {(self.surname).capitalize()}'
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+
+
+if __name__ == '__main__':
+    my_class = Position('иван','сидоров','менеджер',100,20)
+    print(f'Name:     {my_class.name}\n'
+          f'Surname:  {my_class.surname}\n'
+          f'Position: {my_class.position}\n'
+          f'Income:   {my_class._income}\n')
+
+    print(f'Полное имя сотрудника: {my_class.get_full_name()}')
+    print(f'Доход с учётом премии: {my_class.get_total_income()}')
+
+
+
+
