d46124807c25865df9547f5e60f1ef577111e19a
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-11-12 12:37:28 +0300

    Modify exercise-1.py

--- lesson-11/exercise-1/my_venv/exercise-1.py
+++ lesson-11/exercise-1/my_venv/exercise-1.py
@@ -15,32 +15,45 @@ from datetime import datetime
 class Date:
 
     def __init__(self,date):
-        self.__date = date
-
-    @property
-    def date(self):
-        return self.__date
+        self.date = date
 
     @classmethod
-    def get_data(cls):
+    def get_data_1(cls,date):
+        return [int(i) for i in date.split('-')]
 
-        data = [int(i) for i in cls.date.split('-')]
-        print(data)
-    #    if Date.check_data(data[::-1]):
-    #        return data
-    #    else:
-    #        raise ValueError('Ваша дата не верна...')
+    @classmethod
+    def get_data_2(cls,date):
+        return cls.check_data([int(i) for i in date.split('-')])
 
     @staticmethod
     def check_data(data):
         try:
-            datetime(*data)
+            datetime(*data[::-1])
         except ValueError:
-            return False
-        return True
+            return f'Ваша дата \"{"-".join([str(i) for i in data])}\" не верна...'
+        return Date('-'.join([str(i) for i in data]))
+
+    def __str__(self):
+        return f'Дата: {self.date}'
 
 
 if __name__ == '__main__':
     my_class = Date('20-11-2021')
-    print(f'Первый способ: {my_class.get_data()}')
-    #print(f'Второй способ: {Date.get_data("02-05-1990")}')
+    my_list_1 = my_class.get_data_1("31-04-2018")  # Дата не верна
+    my_list_2 = Date.get_data_1("12-05-2001")      # Дата верна
+    print(f'{my_class}  \n'
+          f'{"-" * 24}  \n'
+          f'{my_list_1} \n'
+          f'{my_class.check_data(my_list_1)} \n'
+          f'{"-" * 24}  \n'
+          f'{my_list_2} \n'
+          f'{Date.check_data(my_list_2)} \n'
+          f'{"-" * 24}  \n'
+          f'{Date.get_data_2("01-00-2020")} \n'     # Дата не верна
+          f'{Date.get_data_2("09-09-2020")} \n'     # Дата верна
+          f'{"-" * 24}  \n')
+
+
+
+
+
