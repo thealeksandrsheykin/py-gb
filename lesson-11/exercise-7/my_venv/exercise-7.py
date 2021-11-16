da9bc9489a65c8cf15a61df55399b388c9b7580f
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-11-16 15:59:18 +0300

    Modify exercise-7.py

--- lesson-11/exercise-7/my_venv/exercise-7.py
+++ lesson-11/exercise-7/my_venv/exercise-7.py
@@ -6,3 +6,27 @@
 сложения и умножения комплексных чисел. Проверить работу проекта. Для этого создать экземпляры класса (комплексные числа),
 выполнить сложение и умножение созданных экземпляров. Проверить корректность полученного результата.
 """
+
+
+class Complex:
+    def __init__(self,complex):
+        self.complex = complex
+
+    def __add__(self, other):
+        if isinstance(self.complex and other.complex, complex):
+            return self.complex + other.complex
+        else:
+            return None
+
+    def __mul__(self, other):
+        if isinstance(self.complex and other.complex, complex):
+            return self.complex * other.complex
+        else:
+            return None
+
+if __name__ == '__main__':
+    x = Complex(1+2j)
+    y = Complex(3+7j)
+    print(f'Сложение: {x + y}')
+    print(f'Умножение: {x * y}')
+
