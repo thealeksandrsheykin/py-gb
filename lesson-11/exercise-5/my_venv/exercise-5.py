345ba5efd181480dd9b8e2a560395b2ebd3d5b91
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2021-11-17 19:59:07 +0300

    Update exercise-5.py

--- lesson-11/exercise-5/my_venv/exercise-5.py
+++ lesson-11/exercise-5/my_venv/exercise-5.py
@@ -7,89 +7,100 @@
 а также других данных, можно использовать любую подходящую структуру (например, словарь).
 """
 
-import re
-
+from abc import abstractmethod
 
 class Store:
+
     def __init__(self):
-        self.__store = {}
-        self.__department = {}
+        self.__store = {'Printer':{},'Scanner':{},'Xerox':{}}
 
-    def add_to_store(self,equipment):
-        # Добавляем на склад оборудование
-        self.__store.setdefault(equipment.type_equipment, {})
-        self.__store[equipment.type_equipment].setdefault(equipment.model, []).append(equipment.series)
-        return f'На склад добавлено устройство:\n\t-{equipment}'
+        self.departments = {'IT':{'Printer':[],'Scanner':[],'Xerox':[]},
+                            'Managers':{'Printer':[],'Scanner':[],'Xerox':[]}}
 
-    def del_from_store(self,equipment,department=None):
-        # Извлекаем со склада оборудование
-        model,series = (re.match(r'\S+:\s+(.*)\s+\S+:\s+(.*)', str(equipment))).groups()
+    def add_to_store(self,equipment,qty):
+        model,series = str(equipment).split(':')
+        self.__store[equipment.type_eq].setdefault(model,{})
+        self.__store[equipment.type_eq][model].setdefault(series,0)
+        self.__store[equipment.type_eq][model][series] += qty
+        return f'На склад добавлен {equipment.type_eq}: {model} {series} {qty} шт.'
+
+    def del_to_store(self,equipment,department,qty=1):
+        model,series = str(equipment).split(':')
         try:
-            self.__store[equipment.type_equipment][model].remove(series)
-        except (KeyError,ValueError):
-            return f'На складе устройство ({equipment}) отсутствует.'
+            self.__store[equipment.type_eq][model][series]
+        except KeyError:
+            return f'На складе {equipment.type_eq}: {model} {series} не обнаружен.'
         else:
-            if department:
-                self.__department.setdefault(department,{})
-                self.__department[department].setdefault(equipment.type_equipment,{})
-                self.__department[department][equipment.type_equipment].setdefault(model, []).append(series)
-            if not self.__store[equipment.type_equipment][model]:
-                del self.__store[equipment.type_equipment][model]
-            if not self.__store[equipment.type_equipment]:
-                del self.__store[equipment.type_equipment]
-        return f'Со склада забрали устройство:\n\t-{equipment}'
+            self.__store[equipment.type_eq][model][series] -= qty
+            if self.__store[equipment.type_eq][model][series] == 0:
+                del self.__store[equipment.type_eq][model][series]
+            if not self.__store[equipment.type_eq][model]:
+                del self.__store[equipment.type_eq][model]
+            self.departments[department][equipment.type_eq].append(str(equipment))
 
+        return f'Со склада забрали {equipment.type_eq}: {model} {series} {qty} шт. в {department} отдел.'
 
 
 class Equipments:
+
     def __init__(self,model,series):
         self.model = model
         self.series = series
 
     @property
-    def type_equipment(self):
+    def type_eq(self):
         return self.__class__.__name__
 
+    @abstractmethod
+    def action(self):
+        pass
+
     def __repr__(self):
-        f'Модель: {self.model} Серия: {self.series}'
+        return f'{self.model}:{self.series}'
 
 class Printer(Equipments):
+
     def __init__(self,model,series):
         super().__init__(model,series)
 
     def action(self):
-        return f'Принтер {self.model} {self.series} печатает.'
-
-    def __str__(self):
-        return f'Модель: {self.model} Серия: {self.series}'
-
+        return f'Принтер {self.model} ({self.series}) печатает.'
 
 class Scanner(Equipments):
+
     def __init__(self,model,series):
         super().__init__(model,series)
 
     def action(self):
-        return f'Сканер {self.model} {self.series} сканирует.'
-
-    def __str__(self):
-        return f'Модель: {self.model} Серия: {self.series}'
-
+        return f'Сканер {self.model} ({self.series}) сканирует.'
 
 class Xerox(Equipments):
+
     def __init__(self,model,series):
         super().__init__(model,series)
 
     def action(self):
-        return f'Ксерокс {self.model} {self.series} делает копию.'
-
-    def __str__(self):
-        return f'Модель: {self.model} Серия: {self.series}'
+        return f'Ксерокс {self.model} ({self.series}) копирует.'
 
 
 if __name__ == '__main__':
-
     store = Store()
-    equipment_1 = Printer('HP', 'Laser 107WR 209U7A')
-    store.add_to_store(equipment_1)
-    store.del_from_store(equipment_1,'IT')
+    printer = Printer('HP','LaserJet P1006')
+    printer_2 = Printer('Epson', 'Test 1001')
+    printer_3 = Printer('Epson', 'Test 1002')
+    scanner = Scanner('Epson','WorkForce DS-1630')
+    xerox = Xerox('Canon','PIXMA MG2540S')
+
+    print(store.add_to_store(printer,2))
+    print(store.add_to_store(printer,4))
+    print(store.add_to_store(printer_2, 5))
+    print(store.add_to_store(printer_3, 1))
+    print(store.del_to_store(printer,'IT',2))
+    print(store.del_to_store(printer_3, 'Managers', 1))
+
+
+
+
+
+
 
